001476a43b013eb4d6f7c4f56fb227baaf465f8f
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2022-01-25 20:21:38 +0100

    Complete 1/4 - A window to editing

--- src/com/eneyeitech/TextEditor.java
+++ src/com/eneyeitech/TextEditor.java
@@ -1,20 +1,23 @@
 package com.eneyeitech;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 
 public class TextEditor extends JFrame {
     public TextEditor() {
+        super("The first stage");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
-
         initComponent();
 
+        setLocationRelativeTo(null);
         setVisible(true);
         setLayout(null);
     }
 
     public void initComponent() {
-        JTextArea jTextArea = new JTextArea();
+        JTextComponent jTextArea = new JTextArea();
+        jTextArea.setName("TextArea");
         add(jTextArea);
     }
 }

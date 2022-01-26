44d1af03e9deaf8730d6816cdc5afcddb5a57907
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2022-01-26 19:17:32 +0100

    Complete 3/4 - Menu, please!

--- src/com/eneyeitech/TextEditor.java
+++ src/com/eneyeitech/TextEditor.java
@@ -3,22 +3,60 @@ package com.eneyeitech;
 import javax.swing.*;
 import javax.swing.text.JTextComponent;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class TextEditor extends JFrame {
+    private JTextComponent jTextArea;
+
     public TextEditor() {
         super("Text Editor");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
+
+        initMenuBar();
         initComponent();
 
         setLocationRelativeTo(null);
         setVisible(true);
         //setLayout(null);
+    }
+
+    public void initMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(fileMenu);
+
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.setName("MenuSave");
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        openMenuItem.setName("MenuOpen");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.setName("MenuExit");
+
+        exitMenuItem.addActionListener(evt -> {
+            System.exit(0);
+        });
+
+        saveMenuItem.addActionListener(evt -> {
+
+        });
+
+        openMenuItem.addActionListener(evt -> {
+
+        });
+
+        fileMenu.add(saveMenuItem);
+        fileMenu.add(openMenuItem);
+
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenuItem);
+
     }
 
     public void initComponent() {
@@ -40,7 +78,7 @@ public class TextEditor extends JFrame {
         controlPanel.add(loadButton);
 
 
-        JTextComponent jTextArea = new JTextArea(50,50);
+        jTextArea = new JTextArea(50,50);
         jTextArea.setName("TextArea");
 
         JScrollPane jScrollPane = new JScrollPane(jTextArea);
@@ -55,29 +93,37 @@ public class TextEditor extends JFrame {
 
         saveButton.addActionListener(e -> {
             String fileName = filenameField.getText();
-            if (fileName != null && fileName.trim().length() > 0) {
-                try {
-                    BufferedWriter myWriter = new BufferedWriter(new FileWriter(fileName, StandardCharsets.UTF_8));
-                    myWriter.write(jTextArea.getText());
-                    myWriter.close();
-                    System.out.println("Successfully wrote to the file.");
-                } catch (IOException r) {
-                    System.out.println("An error occurred.");
-                    r.printStackTrace();
-                }
-            }
+            save(fileName);
         });
 
         loadButton.addActionListener(e -> {
             String fileName = filenameField.getText();
-            if (fileName != null && fileName.trim().length() > 0) {
-                File selectedFile = new File(fileName);
-                try {
-                    jTextArea.setText(new String(Files.readAllBytes(selectedFile.toPath())));
-                } catch (IOException r) {
-                    jTextArea.setText(null);
-                }
-            }
+            open(fileName);
         });
+    }
+
+    public void save(String fileName){
+        if (fileName != null && fileName.trim().length() > 0) {
+            try {
+                BufferedWriter myWriter = new BufferedWriter(new FileWriter(fileName, StandardCharsets.UTF_8));
+                myWriter.write(jTextArea.getText());
+                myWriter.close();
+                System.out.println("Successfully wrote to the file.");
+            } catch (IOException r) {
+                System.out.println("An error occurred.");
+                r.printStackTrace();
+            }
+        }
+    }
+
+    public void open(String fileName){
+        if (fileName != null && fileName.trim().length() > 0) {
+            File selectedFile = new File(fileName);
+            try {
+                jTextArea.setText(new String(Files.readAllBytes(selectedFile.toPath())));
+            } catch (IOException r) {
+                jTextArea.setText(null);
+            }
+        }
     }
 }
